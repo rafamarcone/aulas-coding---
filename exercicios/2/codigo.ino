b45183eb9a8b7403af8464287310bf68e19aed47
rafamarcone <101127528+rafamarcone@users.noreply.github.com>
2022-03-21 12:22:48 -0300

    Update codigo.ino

--- exercicios/2/codigo.ino
+++ exercicios/2/codigo.ino
@@ -1,1 +1,15 @@
+void setup()
+{
+  pinMode(4, OUTPUT);//led verde
+  pinMode(3, OUTPUT);//led vermelho
+}
 
+void loop()
+{
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  delay(1000); 
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  delay(1000);
+}
