078a6e76424486e6f5fa647b78cb0ab4e4b8924b
rafamarcone <101127528+rafamarcone@users.noreply.github.com>
2022-03-21 13:04:42 -0300

    Update codigo.ino

--- exercicios/4/codigo.ino
+++ exercicios/4/codigo.ino
@@ -1,1 +1,30 @@
+int LED = 2;
+int BOTAO = 3;
+int estado_botao = 0;
+int VAR = 0;
+int VAR2 = 0;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);//led vermelho
+  pinMode(BOTAO, INPUT);
+  
+}
+void loop()
+{
+  VAR = digitalRead(BOTAO);
+  if(VAR ==1 && VAR2 == 0);
+  {
+    estado_botao = 1 - estado_botao;
+    delay(20);
+  }
+  VAR2=VAR;
+  if(estado_botao == 1){
+    digitalWrite(LED, 1);
+  }
+  else
+  {
+    digitalWrite(LED, 0);
+  }
+}
 
